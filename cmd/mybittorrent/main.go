03a068931e289dc98f6f0b2058505fd98a861ea5
Bram Musuko Panjaitan <bram.musuko@bytedance.com>
2024-11-02 22:14:11 +0700

    feat: use bencode
    Change-Id: I9ed0bc0a51906f521190a18861911417208eaa16

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/jackpal/bencode-go"
 	"os"
-	"strconv"
-	"unicode"
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
@@ -16,39 +16,8 @@ var _ = json.Marshal
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
 func decodeBencode(bencodedString string) (interface{}, error) {
-	if unicode.IsDigit(rune(bencodedString[0])) {
-		var firstColonIndex int
-
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
-		}
-
-		lengthStr := bencodedString[:firstColonIndex]
-
-		length, err := strconv.Atoi(lengthStr)
-		if err != nil {
-			return "", err
-		}
-
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else if bencodedString[0] == 'i' && bencodedString[len(bencodedString)-1] == 'e' {
-		num := bencodedString[1 : len(bencodedString)-1]
-		sign := 1
-		if num[0] == '-' {
-			num = num[1:]
-			sign = -1
-		}
-		i, err := strconv.Atoi(num)
-		if err != nil {
-			return "", err
-		}
-		return i * sign, nil
-	} else {
-		return "", fmt.Errorf("Only strings are supported at the moment")
-	}
+	buf := bytes.NewBufferString(bencodedString)
+	return bencode.Decode(buf)
 }
 
 func main() {
