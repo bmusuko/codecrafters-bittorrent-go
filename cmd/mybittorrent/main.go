8ad2189dd9c41090038ed43c27a0977450cc2d0d
Bram Musuko Panjaitan <bram.musuko@bytedance.com>
2024-11-02 22:03:55 +0700

    feat: support int
    Change-Id: Ifbf2fb3369257e5c3ce3efc2a25cd76296bf6c70

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -34,6 +34,18 @@ func decodeBencode(bencodedString string) (interface{}, error) {
 		}
 
 		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
+	} else if bencodedString[0] == 'i' && bencodedString[len(bencodedString)-1] == 'e' {
+		num := bencodedString[1 : len(bencodedString)-1]
+		sign := 1
+		if num[0] == '-' {
+			num = num[1:]
+			sign = -1
+		}
+		i, err := strconv.Atoi(num)
+		if err != nil {
+			return "", err
+		}
+		return i * sign, nil
 	} else {
 		return "", fmt.Errorf("Only strings are supported at the moment")
 	}
