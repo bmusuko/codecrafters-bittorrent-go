89706555bb5e23e67c242e705e69c770778a73ea
Bram Musuko Panjaitan <bram.musuko@bytedance.com>
2024-11-02 22:30:01 +0700

    feat: announce
    Change-Id: Ide11f75d584ca9628419423ce8a236b87bc3a55f

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -9,14 +9,22 @@ import (
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
+type Detail struct {
+	Length int64 `json:"length"`
+}
+type Info struct {
+	Announce string `json:"announce"`
+	Info     Detail `json:"info"`
+}
+
 // Ensures gofmt doesn't remove the "os" encoding/json import (feel free to remove this!)
 var _ = json.Marshal
 
 // Example:
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
-func decodeBencode(bencodedString string) (interface{}, error) {
-	buf := bytes.NewBufferString(bencodedString)
+func decodeBencode(bencodedString []byte) (interface{}, error) {
+	buf := bytes.NewBuffer(bencodedString)
 	return bencode.Decode(buf)
 }
 
@@ -29,7 +37,7 @@ func main() {
 	if command == "decode" {
 		bencodedValue := os.Args[2]
 
-		decoded, err := decodeBencode(bencodedValue)
+		decoded, err := decodeBencode([]byte(bencodedValue))
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -37,6 +45,23 @@ func main() {
 
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
+	} else if command == "info" {
+		filename := os.Args[2]
+		dat, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		decoded, err := decodeBencode(dat)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		jsonOutput, _ := json.Marshal(decoded)
+		info := new(Info)
+		_ = json.Unmarshal(jsonOutput, info)
+		fmt.Printf("Tracker URL: %s\n", info.Announce)
+		fmt.Printf("Length: %d\n", info.Info.Length)
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
